a3c439e17b628af0d7a0820f423949bc81bf5ec7
Christian Pschorr <c.pschorr@datamints.com>
2021-04-24 23:15:40 +0200

    Übernahme der benötigten Dateien aus der XO Extension

---
+++ Classes/DataProcessing/IconTextProcessor.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the package bk2k/bootstrap-package.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Ps\Xo\DataProcessing;
+
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
+
+class IconTextProcessor implements DataProcessorInterface {
+
+	/**
+	 * @param ContentObjectRenderer $cObj The data of the content element or page
+	 * @param array $contentObjectConfiguration The configuration of Content Object
+	 * @param array $processorConfiguration The configuration of this processor
+	 * @param array $processedData Key/value store of processed data (e.g. to be passed to a Fluid View)
+	 * @return array the processed data as key/value store
+	 */
+	public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData) {
+
+		if(isset($processedData['flexform']) === true && isset($processedData['data']['frame_classes']) === true) {
+			$processedData['data']['frame_classes'] .= ' ce-icon-text--' . $processedData['flexform']['settings']['align'];
+		}
+
+		return $processedData;
+	}
+}
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,68 @@
+<?php
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Icon Text Module von TT-Content
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+	array(
+		'LLL:EXT:ce_icon_text/Resources/Private/Language/locallang_tca.xlf:tx_xo_icon_text.title',
+		'ce_icon_text',
+		'content-textpic'
+	),
+	'CType',
+	'ce_icon_text'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+	'*',
+	'FILE:EXT:ce_icon_text/Configuration/FlexForms/ContentElements/IconText.xml',
+	'ce_icon_text'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['ce_icon_text'] = [
+	'showitem' => '
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.header;header,bodytext,tx_xo_elements,pi_flexform,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.appearance,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.frames;frames,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+			--palette--;;hidden,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
+			--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,
+		--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.extended
+	',
+];
+
+$GLOBALS['TCA']['tt_content']['types']['ce_icon_text']['columnsOverrides']['bodytext']['config'] = [
+	'enableRichtext' => true,
+	'richtextConfiguration' => 'xoDefault',
+];
+
+$GLOBALS['TCA']['tt_content']['types']['ce_icon_text']['columnsOverrides']['tx_xo_elements']['config']['overrideChildTca'] = [
+	'columns' => [
+		'record_type' => [
+			'config' => [
+				'items' => [
+					['LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:tx_xo_domain_model_elements.record_type.icon_text_default', 'icon_text_default'],
+				],
+				'default' => 'icon_text_default'
+			]
+		],
+		'media' => [
+			'label' => 'LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:tx_xo_domain_model_elements.type.icon_text.media'
+		],
+		'description' => [
+			'config' => [
+				'richtextConfiguration' => 'xoMinimal'
+			]
+		],
+	],
+	'types' => [
+		'icon_text_default' => [
+			'showitem' => '
+				l10n_diffsource, record_type, --palette--;;header, link, description, media,
+				--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
+				--palette--;;visibility,
+				--palette--;;access',
+		],
+	]
+];
