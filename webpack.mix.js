43d56a0e3279a4050581ff65e4c5da1b3afb6880
Christian Pschorr <c.pschorr@datamints.com>
2021-04-26 23:39:52 +0200

    Webpack Prozess bereinigt

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,142 +8,25 @@ mix.setPublicPath('.');
 // @see: https://laravel.com/docs/5.6/mix#notifications
 mix.disableSuccessNotifications();
 
-// var spritemap = require('svg-spritemap-webpack-plugin');
-// var iconfont = require('iconfont-plugin-webpack');
-//
-// var ImageminPlugin = require('imagemin-webpack-plugin').default;
-// var imageminMozjpeg = require('imagemin-mozjpeg');
-
-// // Autoload jQuery
-// // @see: https://github.com/JeffreyWay/laravel-mix/blob/master/docs/autoloading.md
-// mix.autoload({
-// 	jquery: ['$', 'window.jQuery']
-// });
-
 // Disable Process CSS Urls
 // @see: https://laravel.com/docs/5.7/mix#working-with-stylesheets
 mix.options({
 	processCssUrls: false
 });
 
-// Shot
-// @see: https://github.com/JeffreyWay/laravel-mix/issues/1086
-// var argv = require('yargs').argv;
-// var shot = null
-// if(argv.env.shot !== undefined) {
-// 	shot = argv.env.shot;
-// }
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
 // mix.js('typo3conf/ext/xna/Resources/Public/Js/xna.js', 'assets/js/xna.js')
-// 	.webpackConfig({
-// 		output: {
-// 			publicPath: '/assets/'
-// 		},
-//
-// 		plugins: [
-// 			new spritemap('typo3conf/ext/xna/Resources/Public/Svg/Sprite/*.svg', {
-// 				output: {
-// 					filename: 'assets/svg/sprite.svg',
-// 					svgo: true,
-// 					svg: {
-// 						sizes: false
-// 					}
-// 				},
-// 				sprite: {
-// 					generate: {
-// 						// Generate <use> tags within the spritemap as the <view> tag will use this
-// 						use: true,
-// 						//
-// 						// // Generate <view> tags within the svg to use in css via fragment identifier url
-// 						// // and add -fragment suffix for the identifier to prevent naming colissions with the symbol identifier
-// 						// view: '-fragment',
-//
-// 						// Generate <symbol> tags within the SVG to use in HTML via <use> tag
-// 						symbol: true
-// 					},
-// 				},
-// 			}),
-//
-// 			new iconfont({
-// 				src: './typo3conf/ext/xna/Resources/Public/Svg/Font', // required - directory where your .svg files are located
-// 				family: 'icons', // optional - the `font-family` name. if multiple iconfonts are generated, the dir names will be used.
-// 				dest: {
-// 					font: './assets/fonts/[family].[type]', // required - paths of generated font files
-// 					css: './typo3conf/ext/xna/Resources/Public/Sass/xna/_icons.scss' // required - paths of generated css files
-// 				},
-// 				watch: {
-// 					pattern: './typo3conf/ext/xna/Resources/Public/Svg/Font/*.svg', // required - watch these files to reload
-// 					cwd: undefined // optional - current working dir for watching
-// 				},
-// 			}),
-//
-// 			new ImageminPlugin({
-// 				test: /\.(jpe?g|png|gif)$/i,
-// 				plugins: [
-// 					imageminMozjpeg({
-// 						quality: 90,
-// 					})
-// 				]
-// 			})
-// 		]
-// 	});
-//
-// mix.copy('typo3conf/ext/xna/Resources/Public/Js/xna-inline.js', 'assets/js/xna-inline.js');
-//
-// mix.copy('./typo3conf/ext/xna/Resources/Public/Images/*', './assets/images')
-// 	.copy('./typo3conf/ext/xna/Resources/Public/Svg/Embed/*', './assets/svg')
-// 	.copy('./typo3conf/ext/xna/Resources/Public/Fonts/*', './assets/fonts')
-// 	.copy('./typo3conf/ext/xna/Resources/Public/Icons/*', './assets/icons');
-
 mix.sass('Resources/Public/Sass/icon-text.scss', '../../../../assets/css/modules/icon-text.css')
-// 	.sass('typo3conf/ext/xna/Resources/Public/Sass/xna.scss', 'assets/css/xna.css')
-// 	.sass('typo3conf/ext/xna/Resources/Public/Sass/xna/modules/text-media.scss', 'assets/css/modules/text-media.css')
-// 	.options({
-// 		postCss: [
-// 			require('postcss-cachebuster'),
-// 			require('postcss-combine-duplicated-selectors')({
-// 				removeDuplicatedProperties: true
-// 			})
-// 		]
-// 	}
-// );
+	.options({
+		postCss: [
+			require('postcss-cachebuster'),
+			require('postcss-combine-duplicated-selectors')({
+				removeDuplicatedProperties: true
+			})
+		]
+	}
+);
 
-// mix.postCss('assets/css/xna.css', 'assets/css', [
-// 	require('postcss-inline-svg')({
-// 		paths: ['./assets/svg']
-// 	})
-// ]);
-
-// mix.sass('typo3conf/ext/xna/Resources/Public/Sass/xna-inline.scss', 'assets/css/xna-inline.css')
-// 	.options({
-// 			postCss: [
-// 				require('postcss-cachebuster'),
-// 				require('postcss-combine-duplicated-selectors')({
-// 					removeDuplicatedProperties: true
-// 				}),
-// 				require('postcss-urlrewrite')({
-// 					imports: true,
-// 					properties: true,
-// 					rules: [
-// 						{ from: '../fonts', to: 'assets/fonts'}
-// 					]
-// 				})
-// 			]
-// 		}
-// 	);
-//
-// if(mix.inProduction() === true) {
-// 	mix.babel('assets/js/xna.js', 'assets/js/xna.js');
-// 	mix.minify(['assets/js/xna.js', 'assets/js/xna-inline.js']);
-// 	mix.minify(['assets/css/xna.css', 'assets/css/xna-inline.css', 'assets/css/modules/text-media.css']);
-// }
+if(mix.inProduction() === true) {
+	// mix.minify(['assets/js/xna.js', 'assets/js/xna-inline.js']);
+	mix.minify(['../../../assets/css/modules/icon-text.css']);
+}
