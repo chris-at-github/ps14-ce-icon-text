6f25c368fc9246e9195e12e1b1681a64a34a5324
Christian Pschorr <c.pschorr@datamints.com>
2021-05-05 16:53:39 +0200

    Clean-Up Variablenbenennung

--- Classes/DataProcessing/ModuleProcessor.php
+++ Classes/DataProcessing/ModuleProcessor.php
@@ -15,13 +15,13 @@ class ModuleProcessor extends \Ps\Xo\DataProcessing\ModuleProcessor implements D
 	];
 
 	/**
-	 * @param ContentObjectRenderer $cObj The data of the content element or page
+	 * @param ContentObjectRenderer $contentObject The data of the content element or page
 	 * @param array $contentObjectConfiguration The configuration of Content Object
 	 * @param array $processorConfiguration The configuration of this processor
 	 * @param array $processedData Key/value store of processed data (e.g. to be passed to a Fluid View)
 	 * @return array the processed data as key/value store
 	 */
-	public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData) {
+	public function process(ContentObjectRenderer $contentObject, array $contentObjectConfiguration, array $processorConfiguration, array $processedData) {
 
 		if(isset($processedData['flexform']) === true && isset($processedData['data']['frame_classes']) === true) {
 			$processedData['data']['frame_classes'] .= ' ce-icon-text--' . $processedData['flexform']['settings']['layout'];
