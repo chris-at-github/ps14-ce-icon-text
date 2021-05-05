52bea29958b0b6a6f383428257c214f6e2c254db
Christian Pschorr <c.pschorr@datamints.com>
2021-05-06 01:26:09 +0200

    Auswertung der Layout Flexform Settings im ModuleProcessor ausgewertet

--- Classes/DataProcessing/ModuleProcessor.php
+++ Classes/DataProcessing/ModuleProcessor.php
@@ -24,7 +24,8 @@ class ModuleProcessor extends \Ps\Xo\DataProcessing\ModuleProcessor implements D
 	public function process(ContentObjectRenderer $contentObject, array $contentObjectConfiguration, array $processorConfiguration, array $processedData) {
 
 		if(isset($processedData['flexform']) === true && isset($processedData['data']['frame_classes']) === true) {
-			$processedData['data']['frame_classes'] .= ' ce-gallery--' . $processedData['flexform']['settings']['layout'];
+			$processedData['data']['frame_classes'] .= ' ce-icon-text--' . $processedData['flexform']['settings']['layout'];
+			$processedData['data']['frame_classes'] .= ' ce-icon-text--' . $processedData['flexform']['settings']['align']['inner'];
 		}
 
 		return $processedData;
