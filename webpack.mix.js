d0d4cccd5431120a5b49ce7a466dac4dc81c75ae
Christian Pschorr <c.pschorr@datamints.com>
2021-05-18 22:57:40 +0200

    Ladereihenfolge für Fluid-Dateien angepasst; package.json und webpack.mix.js bereinigt

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ var mix = require('laravel-mix');
 
 // Stopping at 95% emitting
 // @see: https://github.com/JeffreyWay/laravel-mix/issues/1126
-mix.setPublicPath('.');
+mix.setPublicPath('../../../');
 
 // No Noise
 // @see: https://laravel.com/docs/5.6/mix#notifications
@@ -14,8 +14,7 @@ mix.options({
 	processCssUrls: false
 });
 
-// mix.js('typo3conf/ext/xna/Resources/Public/Js/xna.js', 'assets/js/xna.js')
-mix.sass('Resources/Public/Sass/icon-text.scss', '../../../../assets/css/modules/icon-text.css')
+mix.sass('Resources/Public/Sass/icon-text.scss', 'assets/css/modules/icon-text.css')
 	.options({
 		postCss: [
 			require('postcss-cachebuster'),
@@ -27,6 +26,5 @@ mix.sass('Resources/Public/Sass/icon-text.scss', '../../../../assets/css/modules
 );
 
 if(mix.inProduction() === true) {
-	// mix.minify(['assets/js/xna.js', 'assets/js/xna-inline.js']);
 	mix.minify(['../../../assets/css/modules/icon-text.css']);
 }
